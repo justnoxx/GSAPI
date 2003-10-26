8e20268d78cb7da1220933a1e6a14313babdff39
Stefan Traby <stefan@hello-penguin.com>
2003-10-26 06:44:56 -0800

    initial import of GSAPI 0.1 from CPAN
    git-cpan-module:   GSAPI
    git-cpan-version:  0.1
    git-cpan-authorid: OESI
    git-cpan-file:     authors/id/O/OE/OESI/GSAPI-0.1.tar.gz

---
+++ GSAPI.pm
@@ -0,0 +1,196 @@
+package GSAPI;
+
+use 5.008;
+use strict;
+use warnings;
+use Carp;
+
+require Exporter;
+use AutoLoader;
+
+use Tie::Handle;
+
+our @ISA = qw(Exporter Tie::Handle);
+
+# Items to export into callers namespace by default. Note: do not export
+# names by default without a very good reason. Use EXPORT_OK instead.
+# Do not simply export all your public functions/methods/constants.
+
+# This allows declaration	use GSAPI ':all';
+# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
+# will save memory.
+our %EXPORT_TAGS = ( 'all' => [ qw(
+	
+) ] );
+
+our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+
+our @EXPORT = qw(
+	
+);
+
+our $VERSION = '0.1';
+
+sub AUTOLOAD {
+    # This AUTOLOAD is used to 'autoload' constants from the constant()
+    # XS function.
+
+    my $constname;
+    our $AUTOLOAD;
+    ($constname = $AUTOLOAD) =~ s/.*:://;
+    croak "&GSAPI::constant not defined" if $constname eq 'constant';
+    my ($error, $val) = constant($constname);
+    if ($error) { croak $error; }
+    {
+	no strict 'refs';
+	# Fixed between 5.005_53 and 5.005_61
+#XXX	if ($] >= 5.00561) {
+#XXX	    *$AUTOLOAD = sub () { $val };
+#XXX	}
+#XXX	else {
+	    *$AUTOLOAD = sub { $val };
+#XXX	}
+    }
+    goto &$AUTOLOAD;
+}
+
+require XSLoader;
+XSLoader::load('GSAPI', $VERSION);
+
+# Preloaded methods go here.
+
+# Below is stub documentation for your module. You'd better edit it!
+
+=head1 NAME
+
+GSAPI - Perl extension for accessing GNU Ghostscript
+
+=head1 SYNOPSIS
+
+  use GSAPI;
+  my $inst = GSAPI::new_instance();
+  GSAPI::init_with_args($inst);
+  GSAPI::run_string($inst, "12345679 9 mul pstack quit\n");
+  GSAPI::exit($inst);
+  GSAPI::delete_instance $inst;
+
+=head1 ABSTRACT
+
+GSAPI is an interface to GNU Ghostscript.
+It's mainly written to provide a simply interface to ghostscript
+that works under Win32 and Unix.
+
+=head1 DESCRIPTION
+
+=over 4
+
+=item ($prod, $cpy, $rev, $revd) = revision()
+
+   Returns Product name, Copyright, Revision and Revision Date
+   of the ghostscript release.
+
+=item $inst = new_instance()
+
+   Returns the instance handle.
+
+=item delete_instance($inst)
+
+   Destroys the instance.
+
+=item set_stdio($inst, &stdin, &stdout, &stderr)
+
+   Sets the callbacks for ghostscript I/O
+   stdin gets the maximum number of bytes to return on input and should
+   return a string up to that length.
+   stdout/stderr gets the string passed and they should return the number
+   of bytes written.
+   Example:
+
+   set_stdio $inst,
+             sub { "\n" },
+             sub { print STDOUT $_[0]; length $_[0] },
+             sub { print STDERR $_[0]; lenngth $_[0] };
+
+=item $rc = init_with_args($inst, ...)
+
+   initializes the ghostscript stuff.
+
+=item $rc = exit($inst)
+
+   calls gsapi_exit
+
+=item $rc = run_string($inst, $string, [$user_errors ])
+
+   calls gsapi_run_string_with_length
+
+=item $rc = run_file($inst, $filename, [$user_errors])
+
+   calls gsapi_run_file
+
+=item $rc = run_string_begin($inst, [$user_errors])
+
+   calls gs_run_string_begin
+
+=item $rc = run_string_end($inst, [$user_errors])
+
+   calls gs_run_string_end
+   
+=item $rc = run_string_continue($inst, $string, [$user_errors])
+
+   calls gsapi_run_string_continue
+  
+=item TIEHANDLE $class, [ stdin => &stdin, stdout => &stdout, stderr => stderr, args => [ arg1, arg2, ...]
+
+=cut
+
+sub TIEHANDLE {
+   my ($class,%args) = @_;
+   my $inst = new_instance();
+   $args{stdin} ||= sub { <STDIN> };
+   $args{stdout} ||= sub { print STDOUT $_[0]; length $_[0] };
+   $args{stderr} ||= sub { print STDOUT $_[0]; length $_[0] };
+   $args{args} ||= [];
+   set_stdio($inst, $args{stdin}, $args{stdout}, $args{stderr});
+   init_with_args($inst, @{$args{args}});
+   run_string_begin($inst);
+   bless \$inst, $class;
+}
+
+sub WRITE ($$$$) {
+   my ($ref, $buf, $len, $offs) = @_;
+   run_string_continue($$ref, $buf, $len, $offs);
+}
+
+sub DESTROY ($) {
+   my $r = shift;
+   run_string_end($$r);
+   GSAPI::exit($$r);
+   delete_instance($$r);
+}
+
+=back
+
+=head2 EXPORT
+
+None by default.
+
+=head1 AUTHOR
+
+Stefan Traby, E<lt>stefan@hello-penguin.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2003 by Stefan Traby <stefan@hello-penguin.com>
+Copyright (C) 2003 by KW-Computer Ges.m.b.H Graz, Austria.
+
+This library is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License 2.0 or any later version.
+
+The main reason why this module is not available under dual license (Artistic/GPL)
+is simply the fact that GNU Gostscript is only available under GPL and not
+under Artistic License.
+
+=cut
+
+1;
+__END__
---
+++ GSAPI.xs
@@ -0,0 +1,211 @@
+#include "EXTERN.h"
+#include "perl.h"
+#include "XSUB.h"
+
+#include "ppport.h"
+
+#include "const-c.inc"
+
+#include <iapi.h>
+
+typedef gs_main_instance *GSAPI__instance;
+
+static SV *cb_io[3];
+
+static int
+run_cb(int idx, char *msg, int msglen)
+{
+  dSP;
+  int cnt, rc;
+  ENTER; SAVETMPS; PUSHMARK(SP);
+  if(idx) {
+    // for stdout/stderr string is passed.
+    XPUSHs(sv_2mortal(newSVpvn(msg, msglen)));
+  } else {
+    // for stdin, length is passed.
+    XPUSHs(sv_2mortal(newSViv(msglen)));
+  }
+  PUTBACK;
+  cnt = call_sv(cb_io[idx], G_SCALAR);
+  SPAGAIN;
+  if(cnt != 1)
+    croak("run_cb: function should return one argument");
+  if(idx) {
+        rc = POPi; 
+  } else {
+        SV *sv;
+        char * p;
+        STRLEN len;
+        sv = POPs;
+        p = SvPV(sv, len);
+        if(len > msglen)
+          croak("run_cb: too long string returned.");
+        memcpy(msg, p, len);
+        rc = len;
+  }
+  PUTBACK; FREETMPS; LEAVE;
+  return rc;
+}
+
+static int
+run_stdin(void *caller_handle, char *buf, int len)
+{
+  if(cb_io[0])
+     return run_cb(0, buf, len);
+  return 0;
+}
+
+static int
+run_stdout(void *caller_handle, char *buf, int len)
+{
+  if(cb_io[1])
+     return run_cb(1, buf, len);
+  return 0;
+}
+
+static int
+run_stderr(void *caller_handle, char *buf, int len)
+{
+  if(cb_io[2])
+     return run_cb(2, buf, len);
+  return 0;
+}
+
+MODULE = GSAPI		PACKAGE = GSAPI		
+
+
+INCLUDE: const-xs.inc
+
+void
+revision()
+  PROTOTYPE:
+  PREINIT:
+     gsapi_revision_t rev;
+  PPCODE:
+     gsapi_revision(&rev, sizeof(rev));
+     EXTEND(SP, 4);
+     PUSHs(sv_2mortal(newSVpv(rev.product,0)));
+     PUSHs(sv_2mortal(newSVpv(rev.copyright, 0)));
+     PUSHs(sv_2mortal(newSViv(rev.revision)));
+     PUSHs(sv_2mortal(newSViv(rev.revisiondate)));
+
+GSAPI::instance
+new_instance()
+  PROTOTYPE:
+  PREINIT:
+      gs_main_instance *inst = 0;
+  CODE:
+       gsapi_new_instance(&inst, 0); // we don't need to check rc.
+       RETVAL = inst;
+  OUTPUT:
+       RETVAL
+
+void
+delete_instance(inst)
+        GSAPI::instance inst
+   PROTOTYPE: $
+   CODE:
+        gsapi_delete_instance(inst);
+        
+
+IV
+set_stdio(inst, Fstdin, Fstdout, Fstderr)
+        GSAPI::instance inst
+        SV *Fstdin
+        SV *Fstdout
+        SV *Fstderr
+    PROTOTYPE: $$$$
+    PREINIT:
+        int i;
+    CODE:
+       for(i = 0; i < 3; i++)
+          if(!cb_io[i])
+            cb_io[i] = NEWSV(0,0);
+        sv_setsv(cb_io[0], Fstdin);
+        sv_setsv(cb_io[1], Fstdout);
+        sv_setsv(cb_io[2], Fstderr);
+        RETVAL = gsapi_set_stdio(inst, run_stdin, run_stdout, run_stderr);
+    OUTPUT:
+        RETVAL
+        
+IV
+init_with_args(inst, ...)
+        GSAPI::instance inst
+  PROTOTYPE: $;@
+  PREINIT:
+        int i;
+        char *argv[items];
+  CODE:
+        for(i = 0; i < (items-1); i++) {
+          argv[i] = SvPV(ST(i+1), PL_na);
+        }
+        argv[i] = 0;
+        RETVAL = gsapi_init_with_args(inst, items -1, argv);
+OUTPUT:
+        RETVAL
+        
+IV
+exit(inst)
+        GSAPI::instance inst
+   PROTOTYPE: $
+   CODE:
+        RETVAL = gsapi_exit(inst);
+   OUTPUT:
+        RETVAL
+
+IV
+run_string(inst, sv, ...)
+        GSAPI::instance inst
+        SV *sv
+     ALIAS:
+       run_file = 1
+       run_string_continue = 2
+   PROTOTYPE: $$;$
+   PREINIT:
+        int user_errors = 0; // ??
+        int pexit_code; // ??
+        char *p;
+        STRLEN len;
+   CODE:
+        p = SvPV(sv, len);
+        if(items > 2)
+                user_errors = SvIV(ST(2));
+        switch(ix) {
+          case 0:
+                RETVAL = gsapi_run_string_with_length(inst, p, len, user_errors, &pexit_code);
+                break;
+          case 1:
+                RETVAL = gsapi_run_file(inst, p, user_errors, &pexit_code);
+                break;
+          case 2:
+                RETVAL = gsapi_run_string_continue(inst, p, len, user_errors, &pexit_code);
+                break;
+        }
+     OUTPUT:
+        RETVAL
+
+IV
+run_string_begin(inst, ...)
+        GSAPI::instance inst
+      ALIAS:
+        run_string_end = 1
+      PROTOTYPE: $;$
+      PREINIT:
+        int user_errors = 0;
+        int pexit_code;
+      CODE:
+        if(items >1)
+                user_errors = SvIV(ST(1));
+        switch(ix) {
+          case 0:
+            RETVAL = gsapi_run_string_begin(inst, user_errors, &pexit_code);
+            break;
+          case 1:
+            RETVAL = gsapi_run_string_end(inst, user_errors, &pexit_code);
+            break;
+        }
+     OUTPUT:
+        RETVAL
+
+
+
